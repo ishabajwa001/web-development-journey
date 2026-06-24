4336d885ad719454c04bcca5c2934ee61af8da42
ishabajwa001 <ishabajwa001@gmail.com>
2026-06-24 16:56:10 +0500

    Create without_variable.js

---
+++ JavaScript/Programs/Variable-Datatypes/without_variable.js
@@ -0,0 +1,13 @@
+// Used for comments
+
+/* This is the 
+        multiline comment
+*/
+
+//console.log() //used for printing the output
+console.log(5);
+console.log('&');
+console.log("Hello");
+console.log(true);
+console.log();
+
