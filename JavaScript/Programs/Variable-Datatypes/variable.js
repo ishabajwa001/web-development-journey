5f3ee35a2ec20982060e88cd23285a18f479a2b6
ishabajwa001 <ishabajwa001@gmail.com>
2026-06-24 17:12:12 +0500

    Create variable.js

---
+++ JavaScript/Programs/Variable-Datatypes/variable.js
@@ -0,0 +1,33 @@
+//Variable of int type
+let x = 10;
+console.log(x);
+
+//Float
+let y = 4.555;
+console.log(y);
+
+//Check without let
+a = 55;
+console.log(a);
+
+// String type
+let words = "Hello";
+console.log(words);
+
+//String type with single character and single quotes
+let letter = 'A';
+console.log(letter);
+
+//String with in backticks
+let greeting = `How are you?`;
+console.log(greeting)
+
+//Undefined
+let age;
+console.log(age);
+age = 5;
+console.log(age);
+
+//null
+let name = null;
+console.log(name);
