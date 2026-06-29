dbf1d1cc84eaadc694cdd14e38b245314f162b63
ishabajwa001 <ishabajwa001@gmail.com>
2026-06-29 16:37:41 +0500

    Create string.js

---
+++ JavaScript/Programs/String/string.js
@@ -0,0 +1,13 @@
+let singleQuotes = 'This is a Progam';
+
+let DoubleQuotes = "No, This is a Programme";
+
+//Reassignment
+let machine = "Vacuum Cleaner";
+machine = "Computer";
+
+const Greeting = "Hello, Are you Human?";
+// Greeting = "Hi, How are you?";  //Error because greeting is declared with const
+
+console.log(Greeting);
+console.log("Well, obviously a human. I can't imagine a lion typing, programming, and sorting algorithms.");
