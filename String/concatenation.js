0689da3a652c25f5023fdb44bce4a2b6f07807de
ishabajwa001 <ishabajwa001@gmail.com>
2026-06-29 16:30:09 +0500

    Create concatenation.js

---
+++ String/concatenation.js
@@ -0,0 +1,20 @@
+let firstname = "Sihan";
+let lastname ="Sheikh";
+
+// use + sign for concatenation
+let fullname = firstname+" "+lastname;
+console.log("My name is "+fullname+".");
+
+// use += sign to add it to already assigned variable
+let skills = "Problem Solving";
+skills += ", SOC Analysis";
+skills +=", Python Developer";
+console.log("My skills are "+skills+".");
+
+//use concat method
+let goal1 = "Cybersecurity Professional";
+let goal2 = "Ethical Hacker.";
+console.log("I want to be a "+goal1.concat(" and ",goal2));
+
+
+
