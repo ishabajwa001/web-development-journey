5956ac241d49a91ce67a80bc65101a0a0f7468c9
ishabajwa001 <ishabajwa001@gmail.com>
2026-06-29 17:27:42 +0500

    Create Trivia-bot.js

---
+++ JavaScript/Labs/Trivia-bot.js
@@ -0,0 +1,24 @@
+//from freecodecamp lab 1
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Tom";
+let botLocation = "Virtaul Platform";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is "+botName+" and I live on "+botLocation+".");
+console.log("My favorite programming language is "+favoriteLanguage+".");
+
+let codingFact = favoriteLanguage+" has nothing to do with Java.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage+" is one of the most forgiving languages ever created.";
+
+console.log(codingFact);
+
+codingFact = "Today, "+favoriteLanguage+" powers websites, mobile apps, desktop applications, and servers";
+
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
