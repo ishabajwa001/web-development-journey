fc06e81262b33bf2f4e5163b3e5975b19e65621d
ishabajwa001 <ishabajwa001@gmail.com>
2026-07-01 17:41:51 +0500

    Create type_change.js

---
+++ JavaScript/Programs/Dynamic_type_language/type_change.js
@@ -0,0 +1,17 @@
+let data = "Tom";
+console.log(data);
+
+data = 23;
+console.log(data);
+
+data = true;
+console.log(data);
+
+data = "g";
+console.log(data);
+
+data;
+console.log(data);
+
+data = null;
+console.log(data);
