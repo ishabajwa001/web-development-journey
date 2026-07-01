a78133c832338939cd1f2b1a41265eac9a8f61e4
ishabajwa001 <ishabajwa001@gmail.com>
2026-07-01 17:54:38 +0500

    Create object-type.js

---
+++ JavaScript/Programs/Variable-Datatypes/object-type.js
@@ -0,0 +1,14 @@
+//create an object pet
+let pet = {
+    name : "Fluffy",
+    type : "Cat",
+    age : 3
+};
+
+console.log(pet);
+
+console.log(pet.name);
+
+console.log(pet.age);
+
+console.log(pet.type);
