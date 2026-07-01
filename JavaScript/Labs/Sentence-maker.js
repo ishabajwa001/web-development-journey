7263e71cd96cedeb7ea3897451acd13de5c24ad4
ishabajwa001 <ishabajwa001@gmail.com>
2026-07-01 17:08:57 +0500

    Create Sentence-maker.js

---
+++ JavaScript/Labs/Sentence-maker.js
@@ -0,0 +1,34 @@
+// Declare variables
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+//Assign values to variables
+adjective = "brave";
+noun ="Dragon";
+verb ="excited";
+place ="castle";
+adjective2 ="smooky";
+noun2 ="Marshmallows";
+
+// Create a story using the variables
+let firstStory;
+firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+  " lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("First story: "+ firstStory);
+
+//Reassign values to variables for a second story
+adjective = "grumpy";
+noun = "Chicken";
+verb = "sneezing"
+place = "home";
+adjective2 = "gigantic";
+noun2 = "pizza";
+
+let secondStory;
+secondStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+  " lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("Second story: "+secondStory);
