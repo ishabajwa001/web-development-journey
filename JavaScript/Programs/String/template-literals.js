2970c780a1458c11ecd7cc10707aa0ce81b0ed8c
ishabajwa001 <ishabajwa001@gmail.com>
2026-07-17 11:17:27 +0500

    Create template-literals.js

---
+++ JavaScript/Programs/String/template-literals.js
@@ -0,0 +1,21 @@
+//Variable Declaration
+let title ="MOVIE CARD";
+let name = "Inception";
+let director = "Christopher Nolan";
+let releaseYear = 2010;
+let rating = 8.5;
+let highestRating = 10;
+
+//Movie Card using template literals
+let movieCard = `
+Name : ${name}
+Director : ${director}
+Release Year : ${releaseYear}
+Rating : ${rating}/${highestRating}`;
+
+//Display title and movie card using template literals
+//Also use the expression to calculate the rating percentage
+console.log(`
+    \t\t\t\t"${title}"
+    ${movieCard}
+Rating Percentage : ${(rating / highestRating) * 100} %`);
