32686c2a6fa471f3e970a61a75ed33ad44c6d227
ishabajwa001 <ishabajwa001@gmail.com>
2026-07-17 10:57:29 +0500

    Create Escape-sequences.js

---
+++ JavaScript/Programs/String/Escape-sequences.js
@@ -0,0 +1,20 @@
+let title = "My Pet Profile";
+let name = "Fluffy";
+let type = "Cat";
+let age = 3;
+
+//Use of escape sequences in variable declaration
+// \ one is for escape sequence and another \ is for file path
+let petFilePath = "C:\\Pet\\Fluffy.txt";
+
+//Display the pet profile and use of \t
+console.log("\t\t\t\t\""+title+"\"");
+console.log("Name : "+name);
+console.log("Type : "+type);
+console.log("Age  : "+age);
+
+//Use of \n for new line
+console.log("\nFluffy is a playful little cat.\n");
+
+console.log("Pet file : "+ petFilePath);
+
