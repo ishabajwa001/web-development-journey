ac2730f8e3511bc65cd9ccbebc5bd83cadcd577f
ishabajwa001 <ishabajwa001@gmail.com>
2026-07-17 10:20:16 +0500

    Create index.js

---
+++ JavaScript/Programs/String/index.js
@@ -0,0 +1,13 @@
+//To access the first letter of a string
+let name = "Michael";
+console.log("First letter of name is : "+name[0]);
+
+//To access the last letter of a string
+let lastname = "Jackson";
+console.log("Last letter of lastname is : "+lastname[lastname.length-1]);
+
+//By using the concatenation method
+let fullname = name.concat(" ",lastname);
+console.log("Fullname is : "+fullname);
+console.log("The first letter of fullname is "+fullname[0]+" and the last letter of fullname is "+fullname[fullname.length-1]);
+
