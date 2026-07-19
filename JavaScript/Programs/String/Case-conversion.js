cf14a59d3fc41d4eb490064cb0cf402a33037c68
ishabajwa001 <ishabajwa001@gmail.com>
2026-07-19 11:20:23 +0500

    Create Case-conversion.js

---
+++ JavaScript/Programs/String/Case-conversion.js
@@ -0,0 +1,13 @@
+let message = "Every Great Programmer Starts With One Line Of Code!";
+let mesUpperCase = message.toUpperCase();
+let mesLowerCase = message.toLowerCase();
+
+console.log(`
+Message = ${message}
+Message in UpperCase = ${mesUpperCase}
+Message in LowerCase = ${mesLowerCase}
+Original Message = ${message}`);
+
+//Both will throw an error
+//console.log(toUpperCase());
+//console.log(toLowerCase());
