ac342e7e2468e453a84c3944737b7876a3e7e29c
ishabajwa001 <ishabajwa001@gmail.com>
2026-07-25 19:11:31 +0500

    Create Number-datatype.js

---
+++ JavaScript/Programs/Numbers-and-Operators/Arithmetic-Operators/Number-datatype.js
@@ -0,0 +1,11 @@
+let num1 = 1;
+let num2 = 2.3;
+let num3 = 0;
+let infinity = 1/0;
+let notANumber = "Hello"/2;
+
+console.log(`\nnum1 : ${num1} : type : ${typeof(num1)}\n`);
+console.log(`num2 : ${num2} : type : ${typeof(num2)}\n`);
+console.log(`num3 : ${num3} : type : ${typeof(num3)}\n`);
+console.log(`num4 : ${infinity} : type : ${typeof(infinity)}\n`);
+console.log(`String : ${notANumber} : type : ${typeof(notANumber)}\n`);
