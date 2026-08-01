a89c71b03aa7bfc7fac489b5cd0614386763091a
ishabajwa001 <ishabajwa001@gmail.com>
2026-08-01 23:09:38 +0500

    Create Compound-operators.js

---
+++ JavaScript/Programs/Numbers-and-Operators/Arithmetic-Operators/Compound-operators.js
@@ -0,0 +1,28 @@
+// Compound Operators
+let num1 = 5;
+num1 = num1 + 10;
+console.log(`num1 = ${num1}`);
+
+let num2 = 8;
+num2 += 3;
+console.log(`num2 = ${num2}`);
+
+let num3 = 15;
+num3 -= 5;
+console.log(`num3 = ${num3}`);
+
+let num4 = 20;
+num4 *= 2;
+console.log(`num4 = ${num4}`);
+
+let num5 = 30;
+num5 /= 3;
+console.log(`num5 = ${num5}`);
+
+let num6 = 10;
+num6 %= 3;
+console.log(`num6 = ${num6}`);
+
+let num7 = 4;
+num7 **= 2;
+console.log(`num7 = ${num7}`);
